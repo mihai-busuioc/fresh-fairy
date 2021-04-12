ec1b4c258457144de214b42fbc4bbc1f47758729
Mihai Busuioc <mihai.busuioc@freshfairy.ro>
2021-04-12 14:48:37 +0000

    added  discount on orders over 200

--- cart/context.py
+++ cart/context.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+from django.conf import settings
 
 
 def cart_contents(request):
@@ -6,10 +8,25 @@ def cart_contents(request):
     total = 0
     services_count = 0
 
+    if total < settings.DISCOUNT_THRESHOLD:
+        discount = 0
+        discount_delta = total - settings.DISCOUNT_THRESHOLD
+    else:
+        discount = total * Decimal(settings.DISCOUNT_PERCENTAGE / 100)
+        discount_delta = 0
+
+    grand_total = total - discount
+    
+
     context = {
         'cart_items': cart_items,
         'total': total,
         'services_count': services_count,
+        'discount': discount,
+        'discount_delta': discount_delta,
+        'discount_threshold': settings.DISCOUNT_THRESHOLD,
+        'grand_total': grand_total,
+
     }
 
     return context
--- fresh_fairy/settings.py
+++ fresh_fairy/settings.py
@@ -68,6 +68,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
@@ -143,6 +145,15 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Stripe
+DISCOUNT_THRESHOLD = 200
+DISCOUNT_PERCENTAGE = 20
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'freshfairy@example.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
