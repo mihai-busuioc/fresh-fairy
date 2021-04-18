a904d3397e3aaa3c6fbe7e6b80b3801ea1c0e07f
Mihai Busuioc <mihai.busuioc@freshfairy.ro>
2021-04-18 21:43:09 +0000

    added checkout views and templates

--- fresh_fairy/settings.py
+++ fresh_fairy/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'cart',
     'appointment',
     'checkout',
+
+    # other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'fresh_fairy.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -63,8 +68,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # req by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.context.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
--- fresh_fairy/urls.py
+++ fresh_fairy/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('services/', include('services.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
     path('appointment/', include('appointment.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
