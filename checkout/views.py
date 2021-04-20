2f4fca6840fb74f4df30a2d634e2e167712928dc
Mihai Busuioc <mihai.busuioc@freshfairy.ro>
2021-04-20 20:27:37 +0000

    completed wh

--- checkout/views.py
+++ checkout/views.py
@@ -49,17 +49,14 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_cart = json.dumps(cart)
+            order.save()
             for item_id, item_data in cart.items():
                 try:
                     service = Services.objects.get(id=item_id)
-                    """if isinstance(item_data, int):
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            service=service,
-                            quantity=item_data,
-                        )
-                    else:"""
                     for date, quantity in item_data['items_by_date'].items():
                         order_line_item = OrderLineItem(
                             order=order,
