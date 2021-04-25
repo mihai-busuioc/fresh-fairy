94a26c33929d3d9538b02bfc174675779bb89805
Mihai Busuioc <mihai.busuioc@freshfairy.ro>
2021-04-25 07:00:38 +0000

    Add service functionality finished

--- services/views.py
+++ services/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+
 from .models import Services
 from .forms import ServicesForm
+
 # Create your views here.
 
 
@@ -30,7 +33,18 @@ def service_detail(request, services_id):
 
 def add_service(request):
     """ Add a service to the store """
-    form = ServicesForm()
+    if request.method == 'POST':
+        form = ServicesForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Service added successfully!')
+            return redirect(reverse('add_service'))
+        else:
+            messages.error(
+                request, 'Failed to add service. Please ensure form is valid!')
+    else:
+        form = ServicesForm()
+
     template = 'services/add_service.html'
     context = {
         'form': form,
