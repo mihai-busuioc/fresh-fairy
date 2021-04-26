ec9447ab774ff150d09e0a94ad3f74f709e7dd3c
Mihai Busuioc <mihai.busuioc@freshfairy.ro>
2021-04-26 16:28:07 +0000

    deployment to heroku

--- fresh_fairy/settings.py
+++ fresh_fairy/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-*8*%3jmp1q*^8z4&&!cc#^z9i5h4noy%kek(xk**cij15))fc=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fresh-fairy.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -109,12 +109,17 @@ WSGI_APPLICATION = 'fresh_fairy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
