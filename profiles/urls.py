063c7c0396d6edba07e751ba9d3da14ca1a653df
Mihai Busuioc <mihai.busuioc@freshfairy.ro>
2021-04-27 17:02:44 +0000

    refactored profile app

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>',
          views.order_history, name='order_history'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('orders_history/', views.orders_history, name='orders_history'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -34,6 +34,31 @@ def profile(request):
     return render(request, template, context)
 
 
+@login_required
+def edit_profile(request):
+    """ Display the user's edit profile details """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(
+                request, 'Update failed. Please ensure the form is valid!')
+    else:
+        form = UserProfileForm(instance=profile)
+
+    template = 'profiles/edit_profile.html'
+    context = {
+        'form': form,
+        'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
@@ -46,6 +71,22 @@ def order_history(request, order_number):
     context = {
         'order': order,
         'from_profile': True,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def orders_history(request):
+    """ Display the user's order history. """
+
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
+
+    template = 'profiles/orders_history.html'
+    context = {
+        'orders': orders,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
